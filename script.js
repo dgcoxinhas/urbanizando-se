f4f066f0d095cc5daa25750dae71b3830eba66e9
Duda Chambó <160850761+dgcoxinhas@users.noreply.github.com>
2024-06-19 19:54:38 -0300

    h

---
+++ script.js
@@ -0,0 +1,15 @@
+<script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+
+<script>
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+</script>
+
+<script>
+
+        function abrirMenu() {document.getElementById('menu').style.width = '250px'};
+        
+
+        function fecharMenu() {document.getElementById('menu').style.width = '0px'};
+        
+
+</script>

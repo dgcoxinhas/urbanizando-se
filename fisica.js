de5b1f693ac4e91be92dff2f8769c56e5ba0246e
Duda Chambó <160850761+dgcoxinhas@users.noreply.github.com>
2025-04-17 12:44:48 -0300

    55

---
+++ fisica.js
@@ -0,0 +1,9 @@
+<script>
+
+        function abrirMenu() {document.getElementById('menu').style.width = '250px'};
+        
+
+        function fecharMenu() {document.getElementById('menu').style.width = '0px'};
+        
+
+</script>
